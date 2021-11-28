5698cf7e55431a760cf6eaaa2bb5a66105b97b79
altinthaqi <altin.thaqi.520@gmail.com>
2021-11-28 23:36:09 +0100

    Created components, passed props, structured files"

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,15 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+
+function Expenses({ expenses }) {
+  return (
+    <Card className="expenses">
+      {expenses.map((item) => (
+        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
