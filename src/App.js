8fa62b71eca473a9cbeeb0fa451f429b16e78b9a
altinthaqi <altin.thaqi.520@gmail.com>
2021-11-30 19:09:34 +0100

    Chart and Dynamic Display of data over the year

--- src/App.js
+++ src/App.js
@@ -22,6 +22,25 @@ const DUMMY_EXPENSES = [
     amount: 450,
     date: new Date(2021, 5, 12),
   },
+  {
+    id: "e5",
+    title: "Samsung Note 9",
+    amount: 599.99,
+    date: new Date(2020, 12, 14),
+  },
+  { id: "e6", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e7",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 12, 28),
+  },
+  {
+    id: "e8",
+    title: "New Desk (White)",
+    amount: 450,
+    date: new Date(2021, 10, 12),
+  },
 ];
 
 function App() {
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,12 @@
 import "./Expenses.css";
-
+import ExpensesChart from "./ExpensesChart";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 import ExpensesList from "./ExpensesList";
 
 function Expenses({ items }) {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2021");
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
     console.log(selectedYear);
@@ -24,6 +24,7 @@ function Expenses({ items }) {
           onFilterChange={filterChangeHandler}
         />
 
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </>
